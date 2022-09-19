4b84b50efb09e09b92beedb841474354ca36f989
rinat-90 <ribroff@gmail.com>
2022-09-18 22:22:50 -0500

    test tabs comp

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,11 +1,49 @@
-import React, {ReactElement} from 'react';
+import './Tabs.scss';
+import React, { ReactElement } from 'react';
+import classnames from 'classnames';
+import {TTabItems} from "../../types";
 
-const Tabs = (): ReactElement => {
-  return (
-    <div>
-      tabs
-    </div>
-  );
+export type TTabsProps = {
+  tabs: TTabItems[]
+  changeTab: (val: any) => void
+  active: any
+  textOnly: boolean
+  iconOnly: boolean
+  inset: boolean
+  showActiveText: boolean
+}
+export const Tabs = (props: TTabsProps): ReactElement => {
+  return <div className={classnames('tabs', { 'inset': props.inset })}>
+    {props.tabs.length > 0 && props.tabs.map(tab => (
+      <div
+        key={tab.title}
+        onClick={() => props.changeTab(tab.value)}
+        className={classnames('tabs-item', {
+          'active': props.active === tab.value,
+          'rounded': props.showActiveText,
+        })}
+      >
+        {props.iconOnly && !props.showActiveText
+          ? <span className='icon'>{tab.icon}</span>
+          : props.iconOnly && props.showActiveText
+            ? <>
+              {tab.icon && <span className='icon'>{tab.icon}</span>}
+              {tab.value === props.active && <span className='title'>{tab.title}</span>}
+            </>
+            : props.textOnly ? <span className='title'>{tab.title}</span>
+              :<>
+                {tab.icon && <span className='icon'>{tab.icon}</span>}
+                <span className='title'>{tab.title}</span>
+              </>}
+      </div>
+    ))}
+  </div>;
+
 };
 
-export default Tabs;
+Tabs.defaultProps = {
+  textOnly: false,
+  iconOnly: false,
+  inset: false,
+  showActiveText: false,
+};
