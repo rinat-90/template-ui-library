1a3723fe7ac8472b98824c533a7a27465b6aba3a
rinat-90 <ribroff@gmail.com>
2022-09-24 12:52:55 -0500

    fix: player avatar styles

--- src/components/PlayerAvatar/PlayerAvatar.tsx
+++ src/components/PlayerAvatar/PlayerAvatar.tsx
@@ -19,49 +19,62 @@ type TPlayerAvatarProps = {
   disableChatOnClick?: boolean;
   unreadMessageCount?: number;
 };
-const PlayerAvatar = (props: TPlayerAvatarProps): ReactElement => {
+const PlayerAvatar = ({
+  firstName,
+  lastName,
+  status,
+  avatar,
+  isCaptain,
+  isMvp,
+  addMargin,
+  size,
+  onClick,
+  nameCaption,
+  onPlayerNameClick,
+  alignCenter,
+  disableChatOnClick,
+  unreadMessageCount,
+}: TPlayerAvatarProps): ReactElement => {
 
-  const initials = `${props.firstName.charAt(0)}${props.lastName.charAt(0)}`;
-  const { unreadMessageCount } = props;
+  const initials = `${firstName.charAt(0)}${lastName.charAt(0)}`;
 
   const avatarClass = classNames(
     'player-avatar',
     {
-      confirmed: props.status === 'yes',
-      no: props.status === 'no',
+      confirmed: status === 'yes',
+      no: status === 'no',
     },
-    { 'add-margin': props.addMargin },
+    { 'add-margin': addMargin },
     {
-      tiny: props.size === 'tiny',
-      small: props.size === 'small',
-      medium: props.size === 'medium',
-      large: props.size === 'large',
+      tiny: size === 'tiny',
+      small: size === 'small',
+      medium: size === 'medium',
+      large: size === 'large',
+      capitan: isCaptain,
+      mvp: isMvp,
     },
-    { 'pointer': props.onClick !== undefined},
+    { 'pointer': onClick !== undefined},
   );
 
   const containerClass = classNames('player-avatar-container', {
-    'align-center': props.alignCenter,
+    'align-center': alignCenter,
   });
 
-  if (process?.env?.NODE_ENV === 'production' || props?.disableChatOnClick) {
+  if (process?.env?.NODE_ENV === 'production' || disableChatOnClick) {
     return (
       <div
         className={containerClass} >
-        <div className={avatarClass} onClick={props.onClick}>
-          {props.avatar ? (
-            <img src={props.avatar} alt={`${props.firstName}-${props.lastName}-avatar`} />
+        <div className={avatarClass} onClick={onClick}>
+          {avatar ? (
+            <img src={avatar} alt={`${firstName}-${lastName}-avatar`} />
           ) : (
             initials
           )}
         </div>
-        {props.isCaptain ? (
-          <p className={classNames('captain', { 'margin-right': props.addMargin })}>C</p>
-        ) : null}
-        {props.isMvp && <div className={classNames('mvp', { 'margin-right': props.addMargin })} />}
-        {props.nameCaption && (
-          <div className='player-name' onClick={props.onPlayerNameClick}>
-            {props.firstName.charAt(0)} {props.lastName}
+        {isMvp && <div className={classNames('mvp', { 'margin-right': addMargin })} />}
+        {nameCaption && (
+          <div className='player-name' onClick={onPlayerNameClick}>
+            {firstName.charAt(0)} {lastName}
           </div>
         )}
       </div>
@@ -69,9 +82,9 @@ const PlayerAvatar = (props: TPlayerAvatarProps): ReactElement => {
   } else {
     return (
       <div className={containerClass}>
-        <div className={avatarClass} onClick={props.onClick}>
-          {props.avatar ? (
-            <img src={props.avatar} alt={`${props.firstName}-${props.lastName}-avatar`} />
+        <div className={avatarClass} onClick={onClick}>
+          {avatar ? (
+            <img src={avatar} alt={`${firstName}-${lastName}-avatar`} />
           ) : (
             initials
           )}
@@ -79,11 +92,9 @@ const PlayerAvatar = (props: TPlayerAvatarProps): ReactElement => {
         { unreadMessageCount && unreadMessageCount > 0 ? <i className='notification-count'>
           {unreadMessageCount}
         </i> : null}
-        {props.isCaptain ? <p className={classNames('captain', { 'margin-right': props.addMargin })}>C</p> : null}
-        {props.isMvp && <div className={classNames('mvp', { 'margin-right': props.addMargin })} />}
-        {props.nameCaption && (
-          <div className={`player-name ${props.size}`} onClick={props.onPlayerNameClick}>
-            {props.firstName} {props.lastName.charAt(0)}
+        {nameCaption && (
+          <div className={`player-name ${size}`} onClick={onPlayerNameClick}>
+            {firstName} {lastName.charAt(0)}
           </div>
         )}
       </div>
