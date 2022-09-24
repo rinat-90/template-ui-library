9060eaa53fb1688587d3b45cb010e0d801bb9791
rinat-90 <ribroff@gmail.com>
2022-09-24 17:31:25 -0500

    test: button component

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,9 +1,42 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import Button from './Button';
 
-describe('Tabs', () => {
-  test('render tabs component', () => {
-    render(<Button label={'Button'}  />);
+describe('Button', () => {
+  test('render button component', async () => {
+    const { getByRole } = render(<Button label={'Button'}  />);
+    const button = getByRole('button');
+    expect(button).toHaveTextContent('Button');
+  });
+  test('render primary button', async () => {
+    const { getByRole } = render(<Button label={'Button'} primary={true} />);
+    const button = getByRole('button');
+    expect(button).toHaveClass('button-primary');
+  });
+  test('render small button', async () => {
+    const { getByRole } = render(<Button label={'Button'} size={'small'} />);
+    const button = getByRole('button');
+    expect(button).toHaveClass('button-small');
+  });
+  test('render large button', async () => {
+    const { getByRole } = render(<Button label={'Button'} size={'large'} />);
+    const button = getByRole('button');
+    expect(button).toHaveClass('button-large');
+  });
+  test('render rounded button', async () => {
+    const { getByRole } = render(<Button label={'Button'} rounded />);
+    const button = getByRole('button');
+    expect(button).toHaveClass('button-rounded');
+  });
+  test('render outlined button', async () => {
+    const { getByRole } = render(<Button label={'Button'} outlined />);
+    const button = getByRole('button');
+    expect(button).toHaveClass('button-outlined');
+  });
+  test('render inset button', async () => {
+    const { getByRole } = render(<Button label={'Button'} inset />);
+    const button = getByRole('button');
+    expect(button).toHaveClass('button-inset');
   });
 });
